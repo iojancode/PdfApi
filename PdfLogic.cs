cae9944efb0c9c52d3bcc6422d23cee9bf9bd9ce
Johann M <jmedinap@gmail.com>
2024-11-21 23:53:57 -0500

    multipart implementation

--- PdfLogic.cs
+++ PdfLogic.cs
@@ -2,12 +2,33 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using PuppeteerSharp;
+using PuppeteerSharp.Media;
 
 namespace PdfApi
 {
     static class PdfLogic
     {
-        public static async Task<Stream> FromHtml(string body, string password = null)
+        public static Task<Stream> FromHtml(string body, string password = null)
+        {
+            return FromHtml(new PdfOptions { PrintBackground = true }, body, password);
+        }
+
+        public static Task<Stream> FromHtml(HtmlModel model, string password = null)
+        {
+            var options = new PdfOptions { PrintBackground = true };
+            
+            if (model.HeaderTemplate != null) options.HeaderTemplate = model.HeaderTemplate;
+            if (model.FooterTemplate != null) options.FooterTemplate = model.FooterTemplate;
+            if (model.FooterTemplate != null || model.HeaderTemplate != null) options.DisplayHeaderFooter = true;
+
+            if (model.MarginTop != null || model.MarginBottom != null || model.MarginLeft != null || model.MarginRight != null)
+                options.MarginOptions = new MarginOptions() { 
+                    Top = model.MarginTop, Bottom = model.MarginBottom, Left = model.MarginLeft, Right = model.MarginRight };
+
+            return FromHtml(options, model.Html, password);          
+        }
+
+        private static async Task<Stream> FromHtml(PdfOptions options, string body, string password)
         {
             using var browserFetcher = new BrowserFetcher();
             await browserFetcher.DownloadAsync();
@@ -16,10 +37,10 @@ namespace PdfApi
                 "--no-sandbox", "--disable-setuid-sandbox", "--font-render-hinting=none", "--force-color-profile=srgb" } });
             await using var page = await browser.NewPageAsync();
             await page.SetContentAsync(body);
-            var result = await page.PdfStreamAsync(new PdfOptions { PrintBackground = true });
+            var result = await page.PdfStreamAsync(options);
 
             if (string.IsNullOrWhiteSpace(password)) return result;
-            else return EncryptLogic.EncryptPDFWithQPDF(result, password);
+            else return EncryptLogic.EncryptPDFWithQPDF(result, password);            
         }
     }
 }
